00c651b694b0dc90c1d2f64be74c14b198365ebf
Artyom <Artyom@DESKTOP-HB4QEA4>
2022-05-06 22:53:11 +0300

    Добавьте файлы проекта.

---
+++ Task8/main.cpp
@@ -0,0 +1,11 @@
+﻿#include "Func.h"
+using namespace std;
+int main()
+{
+    Student student;
+    cout << "Surname, name, middle_name, city, group, course?" << endl;
+    cin >> student;
+
+    cout << "You wrote: " << student << '\n';
+    return 0;
+}
